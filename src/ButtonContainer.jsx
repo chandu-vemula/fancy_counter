42b19298f8d56fdeaf98ad960af710b1ae5575aa
chandu-vemula <chanduvemula1996@gmail.com>
2024-11-21 23:51:02 +0530

    Initial commit

---
+++ src/ButtonContainer.jsx
@@ -0,0 +1,12 @@
+import CountButton from "./CountButton";
+
+function ButtonContainer({ setCount, locked }) {
+  return (
+    <div className="button-container">
+      <CountButton type="minus" setCount={setCount} locked={locked} />
+      <CountButton type="plus" setCount={setCount} locked={locked} />
+    </div>
+  );
+}
+
+export default ButtonContainer;
---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Count from "./Count";
+import ButtonContainer from "./ButtonContainer";
+import ResetButton from "./ResetButton";
+import Title from "./Title";
+
+function Card() {
+  const [count, setCount] = useState(0);
+  const locked = count === 5 ? true : false;
+
+  return (
+    <div className={`card ${locked ? "card--limit" : ""}`}>
+      <Title locked={locked} />
+      <Count count={count} />
+      <ResetButton setCount={setCount} />
+      <ButtonContainer setCount={setCount} locked={locked} />
+    </div>
+  );
+}
+
+export default Card;
